bd4fe511ff8b50ebf509c418dfd78027b467c862
Me <designwebmoldova@gmail.com>
2021-04-07 11:17:05 +0300

    1.3.0 I skip version change

--- show-review-posts.php
+++ show-review-posts.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Hapigood reviews plugin
  * Plugin URI:        simpals.com
  * Description:       This is a custom Hapigood plugin for reviews showing
- * Version:           1.2.4
+ * Version:           1.3.0
  * Author:            Simpals Dev
  * Author URI:        simpals.com
  * License:           GPL-2.0+
