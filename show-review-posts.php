ba379d187b10b37d9f5d750c77a171820099df5d
Me <designwebmoldova@gmail.com>
2021-04-08 17:25:26 +0300

    v 1.4.1 change "Write review" logo img

--- show-review-posts.php
+++ show-review-posts.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Hapigood reviews plugin
  * Plugin URI:        simpals.com
  * Description:       This is a custom Hapigood plugin for reviews showing
- * Version:           1.4.0
+ * Version:           1.4.1
  * Author:            Simpals Dev
  * Author URI:        simpals.com
  * License:           GPL-2.0+
